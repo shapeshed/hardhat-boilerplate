64ef8a550f1053f81f160ba4bfe75f803e0fe48e
George Ornbo <george@shapeshed.com>
2021-01-20 15:39:51 +0000

    Update deployment

---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("hai");
+  await greeter.deployed();
+
+  console.log("Greeter address:", greeter.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
