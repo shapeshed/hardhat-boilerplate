95fd377ddd71df146cb986d6bd51862762e82ebc
George Ornbo <george@shapeshed.com>
2021-01-20 15:40:03 +0000

    Update for hardhat

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,14 +1,15 @@
-import { ethers } from "@nomiclabs/buidler";
+import { ethers } from "hardhat";
 import { Signer } from "ethers";
 
-describe("Sample Test", function() {
-  let accounts: Signer[];
+describe("Token", function () {
+    let accounts: Signer[];
 
-  beforeEach(async function() {
-    accounts = await ethers.getSigners();
-  });
-//
-  it("should do something right", async function() {
-    // Do something with the accounts
-  });
-})
+    beforeEach(async function () {
+        accounts = await ethers.getSigners();
+    });
+
+    it("should do something right", async function () {
+        // Do something with the accounts
+    });
+});
+        
