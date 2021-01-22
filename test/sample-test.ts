6be652dd728f45653a5c7bf20f6398f83bebbd9b
George Ornbo <george@shapeshed.com>
2021-01-22 14:06:57 +0000

    Format with prettier

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -3,20 +3,20 @@ import { solidity } from "ethereum-waffle";
 import chai from "chai";
 import mocha from "mocha";
 
-import { Greeter } from '../typechain/Greeter';
+import { Greeter } from "../typechain/Greeter";
 
 chai.use(solidity);
-const { expect} = chai;
+const { expect } = chai;
 
 describe("Greeter", function () {
-    let greeter: Greeter;
-    beforeEach(async()=>{
-        const greeterFactory = await ethers.getContractFactory("Greeter");  
-        greeter = (await greeterFactory.deploy("Hello, world!")) as Greeter;
-        await greeter.deployed();
-    })
+  let greeter: Greeter;
+  beforeEach(async () => {
+    const greeterFactory = await ethers.getContractFactory("Greeter");
+    greeter = (await greeterFactory.deploy("Hello, world!")) as Greeter;
+    await greeter.deployed();
+  });
 
-    it("should do something right", async function () {
-        expect(await greeter.greet()).to.equal("Hello, world!");
-    });
+  it("should do something right", async function () {
+    expect(await greeter.greet()).to.equal("Hello, world!");
+  });
 });
